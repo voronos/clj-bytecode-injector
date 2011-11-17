b85244405969421ef9f56f801fca4ed164fdb0b2
Mark Nelson <nels2426@gmail.com>
2011-11-16 22:41:07 -0600

    modifying some things to make it work inside of an ant task. MAKE SURE you have the ant task fork and that the class path has everything

--- src/profiling_injector/core.clj
+++ src/profiling_injector/core.clj
@@ -1,9 +1,9 @@
 (ns profiling-injector.core
   (:gen-class)
-  (:import (javassist ClassPool CtClass CtMethod)
+  (:import (javassist ClassPool CtClass CtMethod LoaderClassPath)
 	   (java.io File)))
 
-(def classPool (ClassPool/getDefault))
+(def classPool (ClassPool. true))
 (defn get-class
   "Returns a CtClass object for the fully qualified class name"
   [^:String class-name]
@@ -58,7 +58,9 @@
   (str (.getPath dir-file) "/" file-name))
 
 (defn file->class-name [base-dir file]
-  (-> file (.getPath) (.replace File/separatorChar \.) (.replace (str base-dir \.) "") (.replace ".class" "")))
+  (let [base-path (-> base-dir (.replace  File/separatorChar \.) (.replace "//" ".") (.replace \/ \.))
+	class-name (-> file (.getPath) (.replace File/separatorChar \.) (.replace (str base-path \.) "") (.replace ".class" ""))]
+    class-name))
 
 (defn find-classes [base-dir]
   (filter #(.contains (.getName %) ".class")
@@ -68,12 +70,16 @@
 		    (File. base-dir))))
 
 (defn -main
-  "Appends a directory to the default search path and adds logging calls to the specified classes in the path"
+  "Appends a directory to the default search path and adds logging
+  calls to the specified classes in the path. Make sure the ant task
+  is forked and that the class path contains everything you will need
+  to load"
   [class-base-dir & args]
-  (.appendClassPath classPool class-base-dir)
+  (println "Searching " class-base-dir " for classes")
   (doseq [class-name (map (partial file->class-name class-base-dir) (find-classes class-base-dir))]
     (let [ct-class (get-class class-name)]
-      (doseq [method (get-methods ct-class)]
-        (println "modifying method " (.getName ct-class) (.getName method))
-        (add-time-profile method))
+      (when (not (.isInterface ct-class))
+	(doseq [method (get-methods ct-class)]
+	  (println "modifying method " (.getName ct-class) (.getName method))
+	  (add-time-profile method)))
       (.writeFile ct-class class-base-dir))))
